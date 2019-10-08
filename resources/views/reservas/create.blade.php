ac40ecebdca997da6f60b02a95a1b73710f3fe3c
João Vitor <johnbitencourtbr@gmail.com>
2019-10-08 19:45:57 -0300

    view de reservas

--- resources/views/reservas/create.blade.php
+++ resources/views/reservas/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'SIGER - Sistema Gerenciador de Reservas de Equipamentos')
 
 @section('content_header')
-    <h1>Reservas de Equipamentos</h1>
+    <h1>Reservas de Equipamentos:</h1>
 @stop
 
 @section('content')
