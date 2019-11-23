bd1172ba19d868497b1048463ccfec9312baf5ef
João Vitor <johnbitencourtbr@gmail.com>
2019-11-22 23:08:20 -0300

    Revert "Atualização"
    This reverts commit 61d6e5e7fd1e6596a7ac62aed3472325884943b3.

--- resources/views/reservas/index.blade.php
+++ resources/views/reservas/index.blade.php
@@ -22,107 +22,12 @@
   .uper {
     margin-top: 40px;
   }
-
-
-  * {box-sizing: border-box;}
-
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav a.active {
-  background-color: #2196F3;
-  color: white;
-}
-
-.topnav .search-container {
-  float: right;
-}
-
-.topnav input[type=date] {
-  padding: 6px;
-  margin-top: 8px;
-  font-size: 17px;
-  border: none;
-}
-
-.topnav .search-container button {
-  float: right;
-  padding: 6px 10px;
-  margin-top: 8px;
-  margin-right: 16px;
-  background: #ddd;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-}
-
-.topnav .search-container button:hover {
-  background: #ccc;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav .search-container {
-    float: none;
-  }
-  .topnav a, .topnav input[type=text], .topnav .search-container button {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
-  }
-}
 </style>
-
-<div class="card uper">
-    <div class="card-header">
-        <div class="topnav">
-            <a class="active" href="{{ route('reservas.create')}}">Nova Reserva</a>
-   <a href="re-pdf">Gerar relatório </a>
-     <div class="search-container">
-     <form action="{{url('/reservas/busca')}}" method="post">
-      
-     {{ csrf_field() }}
-      <input type="date" placeholder="Buscar.." name="pesquisar" value="{{old('pesquisar')}}">
-       <button type="submit"><i class="fa fa-search"></i></button>
-     </form>
-   </div>
- </div>
-    </div>
-</div>
-
-
-
-</style>
+<div class="uper">
 
   <table class="table table-striped">
-  
+  <a href="{{ route('reservas.create')}}" class="btn btn-primary">Nova Reserva</a> <br><br>
+   
   <div class="container">
         {!! $reservas->render() !!}
     </div>
@@ -166,6 +71,8 @@ body {
     </tbody>
   </table>
 </div>
-
+<div align="right">
+  <a href="re-pdf" class="btn btn-primary">Exportar lista para pdf</a> 
+</div>
 
 @stop
