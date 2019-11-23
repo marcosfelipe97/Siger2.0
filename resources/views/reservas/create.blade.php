61d6e5e7fd1e6596a7ac62aed3472325884943b3
João Vitor <johnbitencourtbr@gmail.com>
2019-11-22 23:08:20 -0300

    Atualização

--- resources/views/reservas/create.blade.php
+++ resources/views/reservas/create.blade.php
@@ -8,15 +8,97 @@
 
 @section('content')
   
-    <style>
+<style>
   .uper {
     margin-top: 40px;
   }
+
+
+  * {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #e9e9e9;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
 </style>
+    
+    
 <div class="card uper">
   <div class="card-header">
-
-  </div>
+    <div class="topnav">
+      <a class="active" href='javascript:create.submit()'>Reservar</a>
+    <a  href="{{ route('reservas.index')}}">Voltar</a>
+    </div>
+      
+   </div>
   <div class="card-body">
     @if ($errors->any())
       <div class="alert alert-danger">
@@ -31,7 +113,7 @@
     
     
       
-      <form method="post" action="{{ route('reservas.store') }}">
+      <form name="create" method="post" action="{{ route('reservas.store') }}">
       {{ csrf_field() }}
          
          
@@ -62,7 +144,9 @@
         
 
 {!!
-          Form::select(
+         
+         
+         Form::select(
                 'equipamentos_id',
                 $equipamentos->pluck('descricao','id'),
                 old('equipamentos_id') ?? request()->get('equipamentos_id'),
@@ -72,9 +156,9 @@
             )
         !!}
 
-          </div>
+        
           
-          <button type="submit" class="btn btn-primary">Confirmar</button>
+         
       </form>
   </div>
 </div>
